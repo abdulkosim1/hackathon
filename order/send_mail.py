efcfa173f61daf512aeecb6abdb587eb6ead0444
Abdulkosim Mashrapov <kasim5151@yandex.ru>
2023-03-21 12:04:37 +0600

    order1

--- order/send_mail.py
+++ order/send_mail.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 
 def send_order_confirmation_code(email, code, name, price):
-    full_link = f'Привет, подтверди заказ на продукт {name} на сумму {price}\n\n http://34.141.58.26//order/confirm/{code}'
+    full_link = f'Привет, подтверди заказ на продукт {name} на сумму {price}\n\n http://34.141.58.26/order/confirm/{code}'
     
     send_mail(
     'Order from FreeHub shop',
